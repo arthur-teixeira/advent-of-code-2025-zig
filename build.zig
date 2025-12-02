37087ab10dc8b9a579e8121d670165fd37adde96
arthur.teixeira <arthurcarvalhot@yahoo.com.br>
2025-12-02 11:26:21 -0300

    Improved build for multiple days

--- build.zig
+++ build.zig
@@ -1,5 +1,18 @@
 const std = @import("std");
 
+pub fn dayModule(b: *std.Build, target: std.Build.ResolvedTarget, common: *std.Build.Module, day: usize) *std.Build.Module {
+    const dayname = b.fmt("day{d:0>2}", .{day});
+    const path = b.fmt("src/day{d:0>2}.zig", .{day});
+
+    return b.addModule(dayname, .{
+        .root_source_file = b.path(path),
+        .target = target,
+        .imports = &.{
+            .{ .name = "common", .module = common },
+        }
+    });
+}
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
@@ -8,14 +21,23 @@ pub fn build(b: *std.Build) void {
         .target = target,
     });
 
-    const mod_day_01 = b.addModule("day01", .{
-        .root_source_file = b.path("src/day01.zig"),
-        .target = target,
-        .imports = &.{
-            .{ .name = "common", .module = mod_common },
-        }
-    });
+    const days = 2;
 
+    var imports: [days]std.Build.Module.Import = undefined;
+    const test_step = b.step("test", "Run tests");
+
+    for (1..days+1) |day| {
+        const dayname = b.fmt("day{d:0>2}", .{day});
+        const module = dayModule(b, target, mod_common, day);
+        imports[day-1] = .{ .name = dayname, .module = module };
+
+        const day_tests = b.addTest(.{
+            .root_module = module,
+        });
+
+        const run_tests = b.addRunArtifact(day_tests);
+        test_step.dependOn(&run_tests.step);
+    }
 
     const exe = b.addExecutable(.{
         .name = "advent_of_code_2025_zig",
@@ -23,9 +45,7 @@ pub fn build(b: *std.Build) void {
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
-            .imports = &.{
-                .{ .name = "day01", .module = mod_day_01 },
-            },
+            .imports = &imports,
         }),
     });
 
@@ -41,13 +61,4 @@ pub fn build(b: *std.Build) void {
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
-    const day01_tests = b.addTest(.{
-        .root_module = mod_day_01,
-    });
-
-    const run_day01_tests = b.addRunArtifact(day01_tests);
-
-    const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_day01_tests.step);
 }
